9b4b3506e8fc6130b42e84214d93874e666a7c6c
fs-dlh <faruk.sari@ai.dlh.lu>
2026-07-14 13:22:52 +0200

    modern_ai task 4 commit

---
+++ data_analysis/data_preparation_visualization/4-remove_duplicates.py
@@ -0,0 +1,16 @@
+#!/usr/bin/env python3
+""" Module for removing duplicate rows from a DataFrame. """
+
+
+
+def remove_duplicates(df):
+    """
+    Remove all duplicate rows from the DataFrame.
+
+    Args:
+        df : DataFrame to deduplicate.
+
+    Returns:
+        df : DataFrame with duplicate rows removed.
+    """
+    return df.drop_duplicates()
