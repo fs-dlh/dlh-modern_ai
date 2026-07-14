15c813af398ab05a59212e60f879e0dc137ae814
fs-dlh <faruk.sari@ai.dlh.lu>
2026-07-14 20:24:22 +0200

    modern_ai task 15 commit

--- data_analysis/data_preparation_visualization/15-encode_features.py
+++ data_analysis/data_preparation_visualization/15-encode_features.py
@@ -17,31 +17,23 @@ def encode_features(df):
     """
     df_enc = df.copy()
 
-    bin_cols = ['SeniorCitizen', 'Partner', 'Dependents', 'PaperlessBilling']
-    ten_col = 'TenureGroup'
-    churn_col = 'Churn'
-    contract_col = 'Contract'
-    payment_col = 'PaymentMethod'
-
     churn_le = preprocessing.LabelEncoder()
-    df_enc[churn_col] = churn_le.fit_transform(df_enc[churn_col])
+    df_enc['Churn'] = churn_le.fit_transform(df_enc['Churn'])
 
-    binary_oe = preprocessing.OrdinalEncoder(
-        categories=[['No', 'Yes']] * len(bin_cols)
-    )
-    df_enc[bin_cols] = binary_oe.fit_transform(df_enc[bin_cols]).astype(int)
-
-    tenure_categories = sorted(df_enc[ten_col].unique())
-    tenure_oe = preprocessing.OrdinalEncoder(categories=[tenure_categories])
-    df_enc[ten_col] = tenure_oe.fit_transform(
-        df_enc[[ten_col]]
-    ).astype(int)
+    bin_cols = ['Partner', 'Dependents', 'PaperlessBilling', 'SeniorCitizen']
+    binary_oe = preprocessing.OrdinalEncoder(categories=[['No', 'Yes']])
+    for c in bin_cols:
+        df_enc[c] = binary_oe.fit_transform(df_enc[[c]])
+        df_enc[c] = df_enc[c].astype(int)
 
     df_enc = pd.get_dummies(
         df_enc,
-        columns=[contract_col, payment_col],
-        drop_first=True,
-        dtype=int
+        columns=['Contract', 'PaymentMethod'], drop_first=True, dtype=int
     )
+
+    tenure_oe = preprocessing.OrdinalEncoder()
+
+    df_enc['TenureGroup'] = tenure_oe.fit_transform(df_enc[['TenureGroup']])
+    df_enc['TenureGroup'] = df_enc['TenureGroup'].astype(int)
 
     return df_enc, churn_le, binary_oe, tenure_oe
