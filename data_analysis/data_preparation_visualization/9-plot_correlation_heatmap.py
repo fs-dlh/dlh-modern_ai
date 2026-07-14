0f59a38417e78a90383a3599d4d978b3b5789026
fs-dlh <faruk.sari@ai.dlh.lu>
2026-07-14 17:08:16 +0200

    modern_ai task 9 commit

---
+++ data_analysis/data_preparation_visualization/9-plot_correlation_heatmap.py
@@ -0,0 +1,30 @@
+#!/usr/bin/env python3
+""" Module for plotting correlation heatmap """
+import seaborn as sns
+import matplotlib.pyplot as plt
+
+
+def plot_correlation_heatmap(df):
+    """ Plot an annotated correlation heatmap for numeric columns.
+
+    Args:
+        df : Input DataFrame.
+
+    Returns:
+        None
+    """
+    plt.figure(figsize=(6, 5))
+
+    numeric_df = df.select_dtypes(include=['float64', 'int64'])
+    corr_matrix = numeric_df.corr()
+
+    sns.heatmap(
+        corr_matrix,
+        annot=True,
+        cmap='coolwarm',
+        vmin=-1,
+        vmax=1
+        )
+
+    plt.tight_layout()
+    plt.show()
