f1af5e376998dca0ae9947450041ef5acd677e32
fs-dlh <faruk.sari@ai.dlh.lu>
2026-07-16 14:00:56 +0200

    Tree based models task2 commit

---
+++ machine_learning/tree_models/2-draw.py
@@ -0,0 +1,23 @@
+#!/usr/bin/env python3
+"""Module to display the textual structure of a trained decision tree. """
+
+from sklearn.tree import export_text
+
+
+def draw(clf, feature_names, class_names):
+    """
+    Print a human-readable textual representation of a trained decision tree.
+
+    Args:
+        clf: A trained DecisionTreeClassifier instance from Scikit-learn.
+        feature_names : List of feature names.
+        class_names : List of target class names.
+
+    Returns:
+        None: The function prints the tree structure to stdout.
+    """
+    tree_text = export_text(
+        clf,
+        feature_names=feature_names,
+        class_names=class_names)
+    print(tree_text)
